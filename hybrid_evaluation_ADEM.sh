b7af6c439a2257e64a28dab84d6157f50f797122
Jasmine WANG <jasminewang76@gmail.com>
2019-02-07 13:35:49 -0500

    updated script

--- hybrid_evaluation_ADEM.sh
+++ hybrid_evaluation_ADEM.sh
@@ -1,6 +1,6 @@
 python hybrid_evaluation.py \
     "eval_ADEM" \
-    -reply_file="tfidf_replies.txt"
+    -reply_file="human_replies.txt"
     # "de_replies.txt"
     # 'tfidf_replies.txt'
     # "human_replies.txt"
