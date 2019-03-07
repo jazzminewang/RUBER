6e3fdbb4a2ba03840c24d7f72ac47b19ea153074
Jasmine Wang <jasminewang76@gmail.com>
2019-03-07 11:37:08 -0500

    updated train script

--- train_twitter.sh
+++ train_twitter.sh
@@ -1,3 +1,3 @@
-python data_helpers.py -dataset="twitter" && \
+# python data_helpers.py -dataset="twitter" && \
 python hybrid_evaluation.py \
-    "twitter" "twitter" "train"
+    "twitter" "twitter" "train" -gru_num_units=128 -init_learning_rate=1 -margin=1 -batch_norm=True
